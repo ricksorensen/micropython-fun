dff8a476e09563d8ed3017e325db92a4476c5457
Rick Sorensen <rick.sorensen@gmail.com>
2023-05-27 09:03:48 -0500

    listpins: Added get_num_pins() and compared with named pins

--- tools/listpins.py
+++ tools/listpins.py
@@ -1,8 +1,23 @@
 from machine import Pin
 import sys
 
+
+def get_num_pins():
+    n = 0
+    for i in range(200):
+        try:
+            _ = Pin(i)
+            n = i
+        except ValueError:
+            break
+    return n + 1
+
+
+n = get_num_pins()
 print(sys.implementation._machine)
+print("Number of pins: {}".format(n))
 if hasattr(Pin, "board"):
+    print("Number of named pins: {}".format(len(Pin.board.__dict__)))
     for p, n in Pin.board.__dict__.items():
         print("{:>10s}: {}".format(p, n))
 else:
