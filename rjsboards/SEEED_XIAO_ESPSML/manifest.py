0f1a1e4711cdd40c198cee61c471f042fe4686a4
Rick Sorensen <rick.sorensen@gmail.com>
2024-10-06 10:26:03 -0500

    ESPSML/manifest.py: remove dht,ds18x20 from manifest due to increased flash size in 5.22/5.3

--- rjsboards/SEEED_XIAO_ESPSML/manifest.py
+++ rjsboards/SEEED_XIAO_ESPSML/manifest.py
@@ -7,8 +7,8 @@ require("bundle-networking")
 
 # Require some micropython-lib modules.
 # require("aioespnow")
-require("dht")
-require("ds18x20")
+# require("dht")
+# require("ds18x20")
 # require("neopixel")
 require("onewire")
 require("umqtt.robust")
