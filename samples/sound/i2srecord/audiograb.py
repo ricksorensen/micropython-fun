2c54318d74d9bc38b3398d1567da5ebed0a6b7f0
Rick Sorensen <rick.sorensen@gmail.com>
2025-03-30 17:12:07 -0500

    audiograb.py: Add grabstereo function
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- samples/sound/i2srecord/audiograb.py
+++ samples/sound/i2srecord/audiograb.py
@@ -59,3 +59,59 @@ def grabmono(
         print(f"    nsamp={nsamp}   samprate={sr}     initdelay={initdelay}")
         print(f"    bytes read={nread}")
     return dout, bfr[:nread], audio_in
+
+
+def grabstereo(
+    nsamp=20000,
+    enc="<",
+    sr=16000,
+    filename="testit.wav",
+    deinit=True,
+    audio_in=None,
+    initdelay=None,
+    verbose=False,
+):
+    #    import array
+    import struct
+
+    gc.collect()
+    bfr = bytearray(nsamp * 4)
+    bfr_mv = memoryview(bfr)
+    if not audio_in:
+        audio_in = I2S(
+            I2S_ID,
+            sck=Pin(SCK_PIN),
+            ws=Pin(WS_PIN),
+            sd=Pin(SD_PIN),
+            mode=I2S.RX,
+            bits=16,  # WAV_SAMPLE_SIZE_IN_BITS,
+            format=I2S.STEREO,
+            rate=sr,
+            ibuf=BUFFER_LENGTH_IN_BYTES,
+        )
+        if initdelay:
+            time.sleep(initdelay)
+        if verbose:
+            print(f"    i2s:{audio_in}")
+
+    nread = audio_in.readinto(bfr_mv)
+    if deinit:
+        audio_in.deinit()
+        audio_in = None
+    #   dout = array.array("h", bfr_mv[:nread])
+    gc.collect()
+    try:
+        dout = struct.unpack(f"{enc}{nread//2}h", bfr_mv[:nread])
+    except MemoryError:
+        gc.collect()
+        print("low on memory")
+        dout = None
+    # print(dout)
+    wavhdr = wavtool.create_wav_header(sr, 16, 2, nsamp)
+    wavtool.writewav(bfr[:nread], wavhdr, filename)
+    gc.collect()
+    if verbose:
+        print(f"audiograb.grabsterei: file={filename}")
+        print(f"    nsamp={nsamp}   samprate={sr}     initdelay={initdelay}")
+        print(f"    bytes read={nread}")
+    return dout, bfr[:nread], audio_in
