da70fc5e64e678d0f480aabee07594b32fd6aca3
Rick Sorensen <rick.sorensen@gmail.com>
2025-04-17 10:20:18 -0500

    rjsboards: rename some board directories
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- rjsboards/SEEED_XIAO_ESPSMLFS/mpconfigboard.cmake
+++
@@ -1,8 +0,0 @@
-set(IDF_TARGET esp32c3)
-
-set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
-    boards/sdkconfig.ble
-    ${MICROPY_BOARD_DIR}/sdkconfig.small.board
-)
